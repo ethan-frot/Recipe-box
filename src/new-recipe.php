134d9dfaba7aa36cb70e1de79de764e1965a0ba8
Ethan <github.xdgcd@passmail.net>
2024-05-23 13:33:14 +0200

    ✨Add back button in forms

--- src/new-recipe.php
+++ src/new-recipe.php
@@ -7,18 +7,25 @@ if (!isset($_SESSION['id'])) {
 
 <?php require_once 'parts/header.php'; ?>
 
-<div class="container">
-  <h1 class="form-title">New recipe</h1>
-
-  <form action="scripts/create-recipe-script.php" method="POST">
     <div class="container">
-      <input required class="form-input" type="text" placeholder="Name" name="recipe_name">
-      <input required class="form-input" type="text" placeholder="Link image (https://)" name="recipe_img">
-      <textarea required rows="4" class="form-input" placeholder="Ingredients (Separate with semi-colon)" name="recipe_ingredients"></textarea>
-      <textarea required rows="4" class="form-input" placeholder="Steps (Separate with semi-colon)" name="recipe_steps"></textarea>
-      <input class="form-button" type="submit" value="Send">
+        <div class="form-title-container">
+            <h1 class="form-title">New recipe</h1>
+            <a class="form-return-button" href="./recipes.php">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+        </div>
+
+        <form action="scripts/create-recipe-script.php" method="POST">
+            <div class="container">
+                <input required class="form-input" type="text" placeholder="Name" name="recipe_name">
+                <input required class="form-input" type="text" placeholder="Link image (https://)" name="recipe_img">
+                <textarea required rows="4" class="form-input" placeholder="Ingredients (Separate with semi-colon)"
+                          name="recipe_ingredients"></textarea>
+                <textarea required rows="4" class="form-input" placeholder="Steps (Separate with semi-colon)"
+                          name="recipe_steps"></textarea>
+                <input class="form-button" type="submit" value="Send">
+            </div>
+        </form>
     </div>
-  </form>
-</div>
 
 <?php require_once 'parts/footer.php'; ?>
--- src/update-recipe.php
+++ src/update-recipe.php
@@ -19,19 +19,29 @@ $request->execute();
 // Fetch all data from table posts
 $recipes = $request->fetch(PDO::FETCH_ASSOC);
 ?>
-<div class="container">
-  <h1 class="form-title">Update recipe</h1>
 
-  <form action="scripts/update-recipe-script.php" method="POST">
-      <div class="container">
-      <input type="number" name="id" id="id" style="display: none;" value="<?= $_GET['id']; ?>">
-      <input required class="form-input" type="text" placeholder="Name" name="recipe_name" value="<?= $recipes['name']; ?>">
-      <input required class="form-input" type="text" placeholder="Link image (https://)" name="recipe_img" value="<?= $recipes['image_src']; ?>">
-      <textarea required rows="4" class="form-input" placeholder="Ingredients (Separate with semi-colon)" name="recipe_ingredients"><?= $recipes['ingredients']; ?></textarea>
-      <textarea required rows="4" class="form-input" placeholder="Steps (Separate with semi-colon)" name="recipe_steps"><?= $recipes['steps']; ?></textarea>
-      <input class="form-button" type="submit" value="Send">
+    <div class="container">
+        <div class="form-title-container">
+            <h1 class="form-title">Update recipe</h1>
+            <a class="form-return-button" href="./recipes.php">
+                <i class="fa-solid fa-chevron-left"></i>
+            </a>
+        </div>
+
+        <form action="scripts/update-recipe-script.php" method="POST">
+            <div class="container">
+                <input type="number" name="id" id="id" style="display: none;" value="<?= $_GET['id']; ?>">
+                <input required class="form-input" type="text" placeholder="Name" name="recipe_name"
+                       value="<?= $recipes['name']; ?>">
+                <input required class="form-input" type="text" placeholder="Link image (https://)" name="recipe_img"
+                       value="<?= $recipes['image_src']; ?>">
+                <textarea required rows="4" class="form-input" placeholder="Ingredients (Separate with semi-colon)"
+                          name="recipe_ingredients"><?= $recipes['ingredients']; ?></textarea>
+                <textarea required rows="4" class="form-input" placeholder="Steps (Separate with semi-colon)"
+                          name="recipe_steps"><?= $recipes['steps']; ?></textarea>
+                <input class="form-button" type="submit" value="Send">
+            </div>
+        </form>
     </div>
-  </form>
-</div>
 
 <?php require_once 'parts/footer.php'; ?>
